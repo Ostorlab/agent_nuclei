23d0f856bce5394d8651aa8605cdcb7f2439f42c
pyzcool <najib.raihan@ostorlab.com>
2022-09-21 18:23:54 +0100

    Fix nuclei agent fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,10 +19,10 @@ def scan_message() -> message.Message:
     """
     selector = 'v3.asset.ip.v4'
     msg_data = {
-            'host': '209.235.136.112',
-            'mask': '32',
-            'version': 4
-        }
+        'host': '209.235.136.112',
+        'mask': '32',
+        'version': 4
+    }
     return message.Message.from_data(selector, data=msg_data)
 
 
@@ -32,10 +32,10 @@ def scan_message_network_range() -> message.Message:
     """
     selector = 'v3.asset.ip.v4'
     msg_data = {
-            'host': '209.235.136.112',
-            'mask': '28',
-            'version': 4
-        }
+        'host': '209.235.136.112',
+        'mask': '28',
+        'version': 4
+    }
     return message.Message.from_data(selector, data=msg_data)
 
 
@@ -45,10 +45,10 @@ def scan_message_large_network_range() -> message.Message:
     """
     selector = 'v3.asset.ip.v4'
     msg_data = {
-            'host': '209.235.136.112',
-            'mask': '16',
-            'version': 4
-        }
+        'host': '209.235.136.112',
+        'mask': '16',
+        'version': 4
+    }
     return message.Message.from_data(selector, data=msg_data)
 
 
@@ -58,9 +58,9 @@ def scan_message_link() -> message.Message:
     """
     selector = 'v3.asset.link'
     msg_data = {
-            'url': 'https://apple.com',
-            'method': 'GET'
-        }
+        'url': 'https://apple.com',
+        'method': 'GET'
+    }
     return message.Message.from_data(selector, data=msg_data)
 
 
@@ -70,14 +70,22 @@ def scan_message_domain() -> message.Message:
     """
     selector = 'v3.asset.domain_name'
     msg_data = {
-            'name': 'example.com'
-        }
+        'name': 'example.com'
+    }
     return message.Message.from_data(selector, data=msg_data)
+
 
 @pytest.fixture
 def nuclei_agent() -> agent.AgentNuclei:
     with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
         definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
+        definition.args = [
+            {
+                'name': 'scope_urls_regex',
+                'value': '([a-zA-Z]+://apple.com/?.*)',
+                'type': 'string'
+            }
+        ]
         settings = runtime_definitions.AgentSettings(
             key='agent/ostorlab/nuclei',
             bus_url='NA',
@@ -99,11 +107,11 @@ def nuclei_agent_args() -> agent.AgentNuclei:
             bus_url='NA',
             bus_exchange_topic='NA',
             args=[
-            utils_definitions.Arg(
-                name='template_urls',
-                type='array',
-                value=json.dumps(['https://raw.githubusercontent.com/Ostorlab/main/templates/CVE1.yaml',
-                      'https://raw.githubusercontent.com/Ostorlab/main/templates/CVE2.yaml']).encode())],
+                utils_definitions.Arg(
+                    name='template_urls',
+                    type='array',
+                    value=json.dumps(['https://raw.githubusercontent.com/Ostorlab/main/templates/CVE1.yaml',
+                                      'https://raw.githubusercontent.com/Ostorlab/main/templates/CVE2.yaml']).encode())],
             healthcheck_port=random.randint(5000, 6000),
             redis_url='redis://guest:guest@localhost:6379')
 
