a0683077de01512d715e5f284cd632a9d1cc86fa
mohsinenar <mnarmouche@gmail.com>
2022-12-07 10:46:51 +0100

    [skip ci] Add black formatter to GitHub workflow.

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,101 +15,73 @@ from agent import agent_nuclei
 
 @pytest.fixture
 def scan_message() -> message.Message:
-    """Creates a dummy message of type v3.asset.ip.v4 to be used by the agent for testing purposes.
-    """
-    selector = 'v3.asset.ip.v4'
-    msg_data = {
-        'host': '209.235.136.112',
-        'mask': '32',
-        'version': 4
-    }
+    """Creates a dummy message of type v3.asset.ip.v4 to be used by the agent for testing purposes."""
+    selector = "v3.asset.ip.v4"
+    msg_data = {"host": "209.235.136.112", "mask": "32", "version": 4}
     return message.Message.from_data(selector, data=msg_data)
 
 
 @pytest.fixture
 def scan_message_network_range() -> message.Message:
-    """Creates a dummy message of type v3.asset.ip.v4 to be used by the agent for testing purposes.
-    """
-    selector = 'v3.asset.ip.v4'
-    msg_data = {
-        'host': '209.235.136.112',
-        'mask': '28',
-        'version': 4
-    }
+    """Creates a dummy message of type v3.asset.ip.v4 to be used by the agent for testing purposes."""
+    selector = "v3.asset.ip.v4"
+    msg_data = {"host": "209.235.136.112", "mask": "28", "version": 4}
     return message.Message.from_data(selector, data=msg_data)
 
 
 @pytest.fixture
 def scan_message_large_network_range() -> message.Message:
-    """Creates a dummy message of type v3.asset.ip.v4 to be used by the agent for testing purposes.
-    """
-    selector = 'v3.asset.ip.v4'
-    msg_data = {
-        'host': '209.235.136.112',
-        'mask': '16',
-        'version': 4
-    }
+    """Creates a dummy message of type v3.asset.ip.v4 to be used by the agent for testing purposes."""
+    selector = "v3.asset.ip.v4"
+    msg_data = {"host": "209.235.136.112", "mask": "16", "version": 4}
     return message.Message.from_data(selector, data=msg_data)
 
 
 @pytest.fixture
 def scan_message_link() -> message.Message:
-    """Creates a dummy message of type v3.asset.link to be used by the agent for testing purposes.
-    """
-    selector = 'v3.asset.link'
-    msg_data = {
-        'url': 'https://apple.com',
-        'method': 'GET'
-    }
+    """Creates a dummy message of type v3.asset.link to be used by the agent for testing purposes."""
+    selector = "v3.asset.link"
+    msg_data = {"url": "https://apple.com", "method": "GET"}
     return message.Message.from_data(selector, data=msg_data)
 
 
 @pytest.fixture
 def scan_message_link_2() -> message.Message:
-    """Creates a dummy message of type v3.asset.link to be used by the agent for testing purposes.
-    """
-    selector = 'v3.asset.link'
-    msg_data = {
-        'url': 'https://ostorlab.co',
-        'method': 'GET'
-    }
+    """Creates a dummy message of type v3.asset.link to be used by the agent for testing purposes."""
+    selector = "v3.asset.link"
+    msg_data = {"url": "https://ostorlab.co", "method": "GET"}
     return message.Message.from_data(selector, data=msg_data)
 
 
 @pytest.fixture
 def scan_message_domain() -> message.Message:
-    """Creates a dummy message of type v3.asset.domain_name to be used by the agent for testing purposes.
-    """
-    selector = 'v3.asset.domain_name'
-    msg_data = {
-        'name': 'apple.com'
-    }
+    """Creates a dummy message of type v3.asset.domain_name to be used by the agent for testing purposes."""
+    selector = "v3.asset.domain_name"
+    msg_data = {"name": "apple.com"}
     return message.Message.from_data(selector, data=msg_data)
 
 
 @pytest.fixture
 def scan_message_domain_2() -> message.Message:
-    """Creates a dummy message of type v3.asset.domain_name to be used by the agent for testing purposes.
-    """
-    selector = 'v3.asset.domain_name'
-    msg_data = {
-        'name': 'ostorlab.co'
-    }
+    """Creates a dummy message of type v3.asset.domain_name to be used by the agent for testing purposes."""
+    selector = "v3.asset.domain_name"
+    msg_data = {"name": "ostorlab.co"}
     return message.Message.from_data(selector, data=msg_data)
 
 
 @pytest.fixture
 def nuclei_agent() -> agent_nuclei.AgentNuclei:
-    with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
+    with (pathlib.Path(__file__).parent.parent / "ostorlab.yaml").open() as yaml_o:
         definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
-        definition.args[4]['value'] = '([a-zA-Z]+://apple.com/?.*)'
+        definition.args[4]["value"] = "([a-zA-Z]+://apple.com/?.*)"
         settings = runtime_definitions.AgentSettings(
-            key='agent/ostorlab/nuclei',
-            bus_url='NA',
-            bus_exchange_topic='NA',
+            key="agent/ostorlab/nuclei",
+            bus_url="NA",
+            bus_exchange_topic="NA",
             args=[],
             healthcheck_port=random.randint(5000, 6000),
-            redis_url='redis://guest:guest@localhost:6379')
+            redis_url="redis://guest:guest@localhost:6379",
+        )
 
         agent_object = agent_nuclei.AgentNuclei(definition, settings)
         return agent_object
@@ -117,15 +89,16 @@ def nuclei_agent() -> agent_nuclei.AgentNuclei:
 
 @pytest.fixture
 def nuclei_agent_no_url_scope() -> agent_nuclei.AgentNuclei:
-    with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
+    with (pathlib.Path(__file__).parent.parent / "ostorlab.yaml").open() as yaml_o:
         definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
         settings = runtime_definitions.AgentSettings(
-            key='agent/ostorlab/nuclei',
-            bus_url='NA',
-            bus_exchange_topic='NA',
+            key="agent/ostorlab/nuclei",
+            bus_url="NA",
+            bus_exchange_topic="NA",
             args=[],
             healthcheck_port=random.randint(5000, 6000),
-            redis_url='redis://guest:guest@localhost:6379')
+            redis_url="redis://guest:guest@localhost:6379",
+        )
 
         agent_object = agent_nuclei.AgentNuclei(definition, settings)
         return agent_object
@@ -133,21 +106,27 @@ def nuclei_agent_no_url_scope() -> agent_nuclei.AgentNuclei:
 
 @pytest.fixture
 def nuclei_agent_args() -> agent_nuclei.AgentNuclei:
-    with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
+    with (pathlib.Path(__file__).parent.parent / "ostorlab.yaml").open() as yaml_o:
         definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
         settings = runtime_definitions.AgentSettings(
-            key='agent/ostorlab/nuclei',
-            bus_url='NA',
-            bus_exchange_topic='NA',
+            key="agent/ostorlab/nuclei",
+            bus_url="NA",
+            bus_exchange_topic="NA",
             args=[
                 utils_definitions.Arg(
-                    name='template_urls',
-                    type='array',
-                    value=json.dumps(['https://raw.githubusercontent.com/Ostorlab/main/templates/CVE1.yaml',
-                                      'https://raw.githubusercontent.com/Ostorlab/main/templates/CVE2.yaml'])
-                    .encode())],
+                    name="template_urls",
+                    type="array",
+                    value=json.dumps(
+                        [
+                            "https://raw.githubusercontent.com/Ostorlab/main/templates/CVE1.yaml",
+                            "https://raw.githubusercontent.com/Ostorlab/main/templates/CVE2.yaml",
+                        ]
+                    ).encode(),
+                )
+            ],
             healthcheck_port=random.randint(5000, 6000),
-            redis_url='redis://guest:guest@localhost:6379')
+            redis_url="redis://guest:guest@localhost:6379",
+        )
 
         agent_object = agent_nuclei.AgentNuclei(definition, settings)
         return agent_object
@@ -156,10 +135,6 @@ def nuclei_agent_args() -> agent_nuclei.AgentNuclei:
 @pytest.fixture
 def ip_small_range_message() -> message.Message:
     """Creates a dummy message of type v3.asset.ip.v4 with a small mask to be used by the agent for testing purposes."""
-    selector = 'v3.asset.ip.v4'
-    msg_data = {
-        'host': '42.42.42.42',
-        'mask': '31',
-        'version': 4
-    }
+    selector = "v3.asset.ip.v4"
+    msg_data = {"host": "42.42.42.42", "mask": "31", "version": 4}
     return message.Message.from_data(selector, data=msg_data)
