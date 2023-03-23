978f5db1e9fff715b6093d77322a3e50030bc8aa
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2023-03-23 11:28:13 +0000

    specify encoding when writing to the files.

--- agent/vpn/wg_vpn.py
+++ agent/vpn/wg_vpn.py
@@ -30,7 +30,7 @@ def enable_vpn_connection(vpn_config: str | None, dns_config: str | None) -> Non
 def _set_vpn_config_file(vpn_config: str) -> None:
     """Set the country configuration file as main one in wireguard."""
     wg_config_path = pathlib.Path(WG_CONF_DEFAULT_PATH)
-    wg_config_path.write_text(data=vpn_config)
+    wg_config_path.write_text(data=vpn_config, encoding="utf-8")
 
 
 def _start_wg() -> None:
@@ -46,4 +46,4 @@ def _start_wg() -> None:
 def _set_wg_dns_config(dns_config: str) -> None:
     """Set wireguard DNS configuration file."""
     dns_config_path = pathlib.Path(DNS_CONFIG_PATH)
-    dns_config_path.write_text(data=dns_config)
+    dns_config_path.write_text(data=dns_config, encoding="utf-8")
