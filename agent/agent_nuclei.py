984b40e1a8d4171df3c0e2f3d59b586b888d5e79
Mohamed Elyousfi <mohamed.elyousfi@ostorlab.dev>
2024-03-12 12:06:20 +0000

    Mark some logs as debug

--- agent/agent_nuclei.py
+++ agent/agent_nuclei.py
@@ -173,13 +173,13 @@ class AgentNuclei(
         if len(targets) > 0:
             templates_urls = self.args.get("template_urls")
             if templates_urls is not None:
-                logger.info("Running custom templates.")
+                logger.debug("Running custom templates.")
                 self._run_templates(templates_urls, targets)
             if self.args.get("use_default_templates", True):
-                logger.info("Running default templates.")
+                logger.debug("Running default templates.")
                 self._run_command(targets)
         self._mark_target_as_processed(message)
-        logger.debug("Done processing message of selector : %s", message.selector)
+        logger.info("Done scanning targets.")
 
     def _parse_output(self) -> None:
         """Parse Nuclei Json output and emit the findings as vulnerabilities"""
@@ -483,7 +483,6 @@ class AgentNuclei(
                 stderr=subprocess.PIPE,
                 check=True,
             )
-            logger.info("Parsing nuclei output.")
             self._parse_output()
         except subprocess.CalledProcessError as e:
             logger.error("Error running nuclei %s", e)
